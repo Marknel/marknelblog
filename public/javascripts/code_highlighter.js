68fc22b94b485a41db1f897d6bd075d4ee64d299
Nathan Hoad <nathan@nathanhoad.net>
2010-06-10 23:15:14 +1000

    Added some more styling to the Code Highlighting for JavaScript

--- public/javascripts/code_highlighter.js
+++ public/javascripts/code_highlighter.js
@@ -299,7 +299,7 @@ CodeHighlighter.addStyle("javascript",{
 		exp  : /\b(arguments|break|case|continue|default|delete|do|else|false|for|function|if|in|instanceof|new|null|return|switch|this|true|typeof|var|void|while|with)\b/
 	},
 	global : {
-		exp  : /\b(toString|valueOf|window|element|prototype|constructor|document|escape|unescape|parseInt|parseFloat|setTimeout|clearTimeout|setInterval|clearInterval|NaN|isNaN|Infinity)\b/
+		exp  : /\b(toString|valueOf|window|element|prototype|constructor|document|escape|unescape|parseInt|parseFloat|setTimeout|clearTimeout|setInterval|clearInterval|NaN|isNaN|Infinity|undefined)\b/
 	}
 });
 
